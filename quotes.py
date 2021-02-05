cd56ef7c88fb6e3c0ec08608c0ff6dc41bec46b5
Vineet Kumar <shubham2vineet@gmail.com>
2021-02-06 04:14:47 +0530

    initial commit

---
+++ quotes.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@localhost/quotes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class favquotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    quote = db.Column(db.String(2000))
+
+@app.route('/')
+def index():
+    result = favquotes.query.all()
+    return render_template('index.html', result=result)
+
+@app.route('/addnew', endpoint='addnew')
+def quotes():
+    return render_template('addnew.html')
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    author = request.form['author']
+    quote = request.form['quote']
+    quotedata = favquotes(author=author, quote=quote)
+    db.session.add(quotedata)
+    db.session.commit()
+    return redirect(url_for('index'))
